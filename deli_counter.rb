a39013ae24017d549b7a37cda83035222fa6c24b
Moose Davis <moosedavis2011@gmail.com>
2020-09-13 16:46:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,16 @@
 katz_deli = []
 
-def line
+def line(array)
+  if line 
 end
+
 
 def take_a_number(array, customer_name)
 end 
+
+take_a_number("Beyonce")
+take_a_number("Billie Eilish")
+take_a_number("Cardi B")
+
 
 def now_serving
