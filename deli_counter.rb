71f06d6e5c9c26f229b0d92a88e67206a42d574a
Moose Davis <moosedavis2011@gmail.com>
2020-09-13 16:55:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,17 @@ katz_deli = []
 def line(array)
   if array == 0 
     puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    
+    array.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+    puts "#{message}"
+  end 
 end
+
+line(katz_deli)
 
 
 def take_a_number(array, customer_name)
