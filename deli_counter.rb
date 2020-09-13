bb64ff14a93612cce1173550b1c26dac68ca4115
Moose Davis <moosedavis2011@gmail.com>
2020-09-13 17:03:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def take_a_number(array, name)
   array.push(name)
-  position = array.indes(name)
+  position = array.index(name)
   puts "Welcome, #{name}.  You are number #{array.index (name)+1} in line."
   return name, position
 end 
